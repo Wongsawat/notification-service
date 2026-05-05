79890c5695f96cb37f4d2346e7d598a00459d455
Weerachat Wongsawat <rabbit_roger@yahoo.com>
2026-05-05 16:56:21 +0700

    feat(notification): add 9 new handler methods to ProcessingEventUseCase interface

--- src/main/java/com/wpanther/notification/application/usecase/ProcessingEventUseCase.java
+++ src/main/java/com/wpanther/notification/application/usecase/ProcessingEventUseCase.java
@@ -1,11 +1,20 @@
 package com.wpanther.notification.application.usecase;
 
+import com.wpanther.notification.application.port.in.event.AbbreviatedTaxInvoiceProcessedEvent;
+import com.wpanther.notification.application.port.in.event.CancellationNoteProcessedEvent;
+import com.wpanther.notification.application.port.in.event.DebitCreditNoteProcessedEvent;
+import com.wpanther.notification.application.port.in.event.DocumentArchivedEvent;
 import com.wpanther.notification.application.port.in.event.EbmsSentEvent;
 import com.wpanther.notification.application.port.in.event.InvoiceProcessedEvent;
 import com.wpanther.notification.application.port.in.event.InvoicePdfGeneratedEvent;
 import com.wpanther.notification.application.port.in.event.PdfSignedEvent;
+import com.wpanther.notification.application.port.in.event.ReceiptProcessedEvent;
+import com.wpanther.notification.application.port.in.event.ReceiptPdfGeneratedEvent;
 import com.wpanther.notification.application.port.in.event.TaxInvoicePdfGeneratedEvent;
 import com.wpanther.notification.application.port.in.event.TaxInvoiceProcessedEvent;
+import com.wpanther.notification.application.port.in.event.CancellationNotePdfGeneratedEvent;
+import com.wpanther.notification.application.port.in.event.DebitCreditNotePdfGeneratedEvent;
+import com.wpanther.notification.application.port.in.event.AbbreviatedTaxInvoicePdfGeneratedEvent;
 import com.wpanther.notification.application.port.in.event.XmlSignedEvent;
 
 /**
@@ -29,4 +38,22 @@ public interface ProcessingEventUseCase {
     void handleXmlSigned(XmlSignedEvent event);
 
     void handleEbmsSent(EbmsSentEvent event);
+
+    void handleReceiptProcessed(ReceiptProcessedEvent event);
+
+    void handleCancellationNoteProcessed(CancellationNoteProcessedEvent event);
+
+    void handleDebitCreditNoteProcessed(DebitCreditNoteProcessedEvent event);
+
+    void handleAbbreviatedTaxInvoiceProcessed(AbbreviatedTaxInvoiceProcessedEvent event);
+
+    void handleReceiptPdfGenerated(ReceiptPdfGeneratedEvent event);
+
+    void handleCancellationNotePdfGenerated(CancellationNotePdfGeneratedEvent event);
+
+    void handleDebitCreditNotePdfGenerated(DebitCreditNotePdfGeneratedEvent event);
+
+    void handleAbbreviatedTaxInvoicePdfGenerated(AbbreviatedTaxInvoicePdfGeneratedEvent event);
+
+    void handleDocumentArchived(DocumentArchivedEvent event);
 }
